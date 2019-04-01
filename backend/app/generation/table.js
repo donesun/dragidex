0fee1d5d15eac5cb37fa2e7807fe87b59eeef514
unknown <sunnymoun93@gmail.com>
2019-03-31 22:27:24 -0700

    added promise to GenerationTable. Can return a generationId from inner method via promises resolve parameter

--- backend/app/generation/table.js
+++ backend/app/generation/table.js
@@ -5,16 +5,22 @@ const pool = require("../../databasePool");
 class GenerationTable {
   // collect related methods for javascript
   static storeGeneration(generation) {
-    //make storeGeneration function static so we dont have to directly make an instance. we can use the function directly with storeGeneration  (this.generation)
-    pool.query(
-      // goal is to insert data+generation entry into table.
-      "INSERT INTO generation(expiration) VALUES($1) ", //schema for data we're going to insert is generation (expiration) $1 is the index of 1. PG index starts at 1 not 0.
-      [generation.expiration], //array of generation.expiration value that will be inserted into our query string
-      (error, response) => {
-        // final parameter of the query, is error,response callback function.
-        if (error) return console.error(error);
-      }
-    );
+    return new Promise((resolve, reject) => {
+      //make storeGeneration function static so we dont have to directly make an instance. we can use the function directly with storeGeneration  (this.generation)
+      pool.query(
+        // goal is to insert data+generation entry into table.
+        "INSERT INTO generation(expiration) VALUES($1) RETURNING id", //schema for data we're going to insert is generation (expiration) $1 is the index of 1. PG index starts at 1 not 0.
+        [generation.expiration], //array of generation.expiration value that will be inserted into our query string
+        (error, response) => {
+          // final parameter of the query, is error,response callback function.
+          if (error) reject(error);
+
+          const generationId = response.rows[0].id; //this gives generations an ID with response.rows method. ---
+
+          resolve({ generationId });
+        }
+      );
+    });
   }
 }
 module.exports = GenerationTable;
